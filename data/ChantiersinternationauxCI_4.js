fdee6c52676673f0a439119ffa130ed4235589ed
leaemery <77677379+leaemery@users.noreply.github.com>
2021-03-04 10:36:02 +0100

    Add files via upload

---
+++ data/ChantiersinternationauxCI_4.js
@@ -0,0 +1,15 @@
+var json_ChantiersinternationauxCI_4 = {
+"type": "FeatureCollection",
+"name": "ChantiersinternationauxCI_4",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "DISPOSITIF": "CHANTIERS INTERNATIONAUX", "ORGANISME": "Concordia Picardie NPDC", "ADRESSE": "174 Rue Franklin Roosevelt  80080 Amiens", "PERSONNE": "Clement LEFEBVRE", "CONTACT": "picardie.npdc@concordia.fr\/03 22 39 06 47" }, "geometry": { "type": "Point", "coordinates": [ 2.298846127104137, 49.909727565667673 ] } },
+{ "type": "Feature", "properties": { "DISPOSITIF": "CHANTIERS INTERNATIONAUX", "ORGANISME": "Service Civil International   Nord", "ADRESSE": "75 Rue du Chevalier Francais  59000 Lille", "PERSONNE": "Loic MARTIN", "CONTACT": "scinord@scinord.fr\/03 20 55 22 58" }, "geometry": { "type": "Point", "coordinates": [ 3.086903340622516, 50.64474735547833 ] } },
+{ "type": "Feature", "properties": { "DISPOSITIF": "CHANTIERS INTERNATIONAUX", "ORGANISME": "Service Volontaire International", "ADRESSE": "75 Rue Leon Gambetta  59000 Lille", "PERSONNE": "NA", "CONTACT": "info@servicevolontaire.fr\/03 66 72 90 20" }, "geometry": { "type": "Point", "coordinates": [ 3.058289871310465, 50.630780905446883 ] } },
+{ "type": "Feature", "properties": { "DISPOSITIF": "CHANTIERS INTERNATIONAUX", "ORGANISME": "Club du vieux manoir (CVM)", "ADRESSE": "Ancienne Abbaye du Moncel 60700 Pontpoint", "PERSONNE": "NA", "CONTACT": "03 44 72 33 98" }, "geometry": { "type": "Point", "coordinates": [ 2.612682771262708, 49.301789871766644 ] } },
+{ "type": "Feature", "properties": { "DISPOSITIF": "CHANTIERS INTERNATIONAUX", "ORGANISME": "ASPAM", "ADRESSE": "1 Place Roger Amboise 02200 Berzy le Sec", "PERSONNE": "NA", "CONTACT": "03 20 53 98 85" }, "geometry": { "type": "Point", "coordinates": [ 3.312483069411374, 49.333421039249373 ] } },
+{ "type": "Feature", "properties": { "DISPOSITIF": "CHANTIERS INTERNATIONAUX", "ORGANISME": "Les blongios", "ADRESSE": "5 Rue Jules de Vicq 59800 Lille", "PERSONNE": "NA", "CONTACT": "contact@lesblongios.fr\/03 20 53 98 85" }, "geometry": { "type": "Point", "coordinates": [ 3.086459873163006, 50.632669193946711 ] } },
+{ "type": "Feature", "properties": { "DISPOSITIF": "CHANTIERS INTERNATIONAUX", "ORGANISME": "Les amis du chateau de Ham", "ADRESSE": "Les Bains Douches  4 Rue Andre Audinot  80400 Ham", "PERSONNE": "NA", "CONTACT": "03 23 81 16 16" }, "geometry": { "type": "Point", "coordinates": [ 3.07173245593427, 49.744297178308059 ] } },
+{ "type": "Feature", "properties": { "DISPOSITIF": "CHANTIERS INTERNATIONAUX", "ORGANISME": "AMVCC", "ADRESSE": "9 Rue du Pot d'etain 02380 Coucy le Chateau", "PERSONNE": "Adeline KADRI", "CONTACT": "adeline.kadri@amvcc.com\/03 23 52 69 41" }, "geometry": { "type": "Point", "coordinates": [ 3.323906140582015, 49.520835346896476 ] } }
+]
+}
